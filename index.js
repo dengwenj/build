7d011472ba14d3d3e0ff4e1fbd1ee4b173b71acc
dengwenj <927561237@qq.com>
2022-04-05 19:11:54 +0800

    图片处理

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import './test/index.css'
+import img1 from './test/我在人间凑数的日子.jpeg'
 
 const message = 'dengwenjie'
 console.log(message)
@@ -6,3 +7,7 @@ console.log(message)
 import('./test/hhh.js').then((res) => {
   console.log(res)
 })
+
+const imgEl = document.createElement('img')
+imgEl.src = img1
+document.body.appendChild(imgEl)
