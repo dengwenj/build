0eb732b7a78e3ddf51259c9ab9cdc05cb1ff90c4
dengwenj <927561237@qq.com>
2022-04-05 18:37:51 +0800

    合并config

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const message = 'dengwenjie'
 console.log(message)
 
+console.log('邓文杰');
+
 import('./test/hhh.js').then((res) => {
   console.log(res)
 })
--- package/react-cli/core/webpack.common.js
+++ package/react-cli/core/webpack.common.js
@@ -2,42 +2,52 @@ const { merge } = require('webpack-merge')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const { DefinePlugin } = require('webpack')
 
 const resolveApp = require('../utils/paths')
+const production = require('./webpack.production')
+const development = require('./webpack.development')
+
+const commonConfig = {
+  target: ['browserslist'],
+  entry: resolveApp('index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: resolveApp('dist')
+  },
+  resolve: {
+    fallback: {
+      path: require.resolve("path-browserify") 
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'react app for dengwj',
+      template: resolveApp('public/index.html')
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: resolveApp('public'),
+          globOptions: {
+            // 要忽略的文件
+            ignore: [
+              "**/index.html"
+            ]
+          }
+        }
+      ]
+    }),
+    new DefinePlugin({
+      BASE_URL: '"./"'
+    })
+  ]
+}
 
 module.exports = function (env) {
-  return {
-    target: ['browserslist'],
-    mode: 'development',
-    entry: resolveApp('index.js'),
-    output: {
-      filename: 'bundle.js',
-      path: resolveApp('dist')
-    },
-    resolve: {
-      fallback: {
-        path: require.resolve("path-browserify") 
-      }
-    },
-    plugins: [
-      new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
-        title: 'react app for dengwj',
-        template: resolveApp('public/index.html')
-      }),
-      new CopyWebpackPlugin({
-        patterns: [
-          {
-            from: resolveApp('public'),
-            globOptions: {
-              // 要忽略的文件
-              ignore: [
-                '**/index.html'
-              ]
-            }
-          }
-        ]
-      })
-    ]
-  }
+  const isProduction = env.production
+  const config = isProduction ? production : development
+
+  return merge(commonConfig, config)
 }
