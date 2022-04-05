776fbc5929ac6e413e19c8b46ac13c6fe5e5b355
dengwenj <927561237@qq.com>
2022-04-05 22:15:14 +0800

    测试styled-component

---
+++ test/styled.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const DWJ = styled.div`
+  font-size: 50px;
+  color: red;
+`
--- test/ts.tsx
+++ test/ts.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react"
 
+import { DWJ } from './styled'
+
 export default function App() {
   const [count, setCount] = useState(0)
 
@@ -7,9 +9,9 @@ export default function App() {
   console.log(name)
 
   return (
-    <>
+    <DWJ>
       <h2>{count}</h2>
       <button onClick={() => setCount(count + 1)}>点击+1</button>
-    </>
+    </DWJ>
   )
 }
